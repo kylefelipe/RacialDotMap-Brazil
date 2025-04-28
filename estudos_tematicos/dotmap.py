88dce5cd1e475183fd220a4d2b861901640c4900
Kyle Felipe <kylefelipe@gmail.com>
2025-04-28 12:52:38 -0300

    refatorando

--- estudos_tematicos/dotmap.py
+++ estudos_tematicos/dotmap.py
@@ -5,10 +5,21 @@ from math import pow
 import geopandas as gpd
 import pandas as pd
 
+import argparse
+
 # Constantes
 A = 1000.0
 
-zoom_levels = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+zoom_levels = [*range(4, 20, 2)]  # Níveis de zoom de 6 a 18
+
+
+race_colors = {
+    "Brancos": "#ff7f9b",
+    "Pretos": "#071119",
+    "Amarelas": "#dda625",
+    "Pardas": "#cfc2ad",
+    "Indígenas": "#1775e1",
+}
 
 
 # Classe para manipular projeções (GlobalMercator em Java)
@@ -69,29 +80,40 @@ class PersonPoint:
         self.quadnode = row[3]
 
 
+# Função que converte cores em hexadecimal para RGB
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i : i + 2], 16) for i in (0, 2, 4))
+
+
 # Processamento principal
-def main():
+def create_tile(gpkg_path):
     proj = GlobalMercator()
     # zoom_levels = [int(line.strip()) for line in open("zoomlevel.txt")]
-
+    # verifica se o arquivo existe
+    print("Starting up...")
+    if not os.path.exists(gpkg_path):
+        print(f"Arquivo {gpkg_path} não encontrado.")
+        return
     for level in zoom_levels:
-        print("Starting up...")
-        gdf = gpd.read_file(
-            "/home/kylefelipe/mapa_repo/estudos_tematicos/acre_pessoas_simples_quadkey.gpkg",
-            # rows=10,
-        )
-        # with open("sorted.csv", newline="") as csvfile: # remover
-        #     reader = csv.reader(csvfile) # remover
-        #     next(reader)  # Ignora o cabeçalho, se houver remover
+        print(f"Processando o nível {level}...")
+        gdf = gpd.read_file(gpkg_path)
 
         quadkey = ""
         image = None
         tms_tile = None
+        # ultimo id do gdf
+        last_id = gdf.index[-1]
 
         for _idx, row in gdf.iterrows():
             px, py = row["geometry"].x / A, row["geometry"].y / A
             new_quadkey = row["quadkey"][:level]
-            race_type = row["raca"]
+            race_type = row["raca_cor"]
+            eol = ""
+            if _idx % 1000 == 0 and quadkey != "":
+                eol = "\n" if _idx == last_id else ""
+                # imprime o texto "Linhas processadas: {linhas}" substindo a linha atual no console pelo novo texto
+                print(f"\rLinhas processadas: {_idx}", end=eol)
 
             if new_quadkey != quadkey:  # Novo tile
                 if image:
@@ -100,30 +122,37 @@ def main():
                     path = f"tiles/{level}/{int(gtile[0])}/{int(gtile[1])}.png"
                     os.makedirs(os.path.dirname(path), exist_ok=True)
                     image.save(path)
+                    if eol == "":
+                        print()
                     print(f"Tile salvo em: {path}")
 
                 quadkey = new_quadkey
                 google_tile = proj.QuadKeyToTileXY(new_quadkey)
                 tms_tile = proj.GoogleTile(google_tile[0], google_tile[1], level)
-                print(f"Novo tile: Nível {level}, {tms_tile}")
+                if eol == "":
+                    print()
+                print(f"Salvando tile: Nível {level}, {tms_tile}")
 
                 image = Image.new("RGBA", (512, 512), (255, 255, 255, 255))
                 draw = ImageDraw.Draw(image)
 
             # Define a cor do ponto ['parda' 'indigena' 'branca' 'amarela' 'preta']
-
-            if race_type == "branca":  # Brancos
-                color = (115, 178, 255, transparent(level))
-            elif race_type == "preta":  # Pretos
-                color = (255, 0, 0, transparent(level))
-            elif race_type == "amarela":  # Asiáticos
-                color = (255, 170, 0, transparent(level))
-            elif race_type == "parda":  # Pardos
-                color = (159, 212, 0, transparent(level))
-            elif race_type == "indigena":  # Indígenas
-                color = (153, 102, 51, transparent(level))
-            else:
+            if race_type not in race_colors.keys():
                 continue
+
+            color = (*hex_to_rgb(race_colors[race_type]), transparent(level))
+            # if race_type == "Brancos":  # Brancos
+            #     color = (115, 178, 255, transparent(level))
+            # elif race_type == "Pretos":  # Pretos
+            #     color = (255, 0, 0, transparent(level))
+            # elif race_type == "Amarelas":  # Asiáticos
+            #     color = (255, 170, 0, transparent(level))
+            # elif race_type == "Pardas":  # Pardos
+            #     color = (159, 212, 0, transparent(level))
+            # elif race_type == "Indígenas":  # Indígenas
+            #     color = (153, 102, 51, transparent(level))
+            # else:
+            #     continue
 
             # Desenha o ponto na imagem
             if image:
@@ -135,7 +164,23 @@ def main():
             path = f"tiles/{level}/{int(gtile[0])}/{int(gtile[1])}.png"
             os.makedirs(os.path.dirname(path), exist_ok=True)
             image.save(path)
+            if eol == "":
+                print()
             print(f"Último tile salvo em: {path}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Processa dados geográficos.")
+    # Adicionar um argumento para o caminho do arquivo GPKG, obrigatório que pode ser --gpkg-path ou -g
+    parser.add_argument(
+        "--gpkg-path",
+        "-g",
+        type=str,
+        required=True,
+        help="Caminho para o arquivo GPKG a ser processado.",
+    )
+    args = parser.parse_args()
+    create_tile(args.gpkg_path)
 
 
 if __name__ == "__main__":
